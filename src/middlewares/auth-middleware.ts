4db9de79f10ed8f1f92edcd340ff6f1342776595
larisasirokih <lorashirokikh@yandex.ru>
2022-08-12 13:25:09 +0300

    new all for new homework

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -21,6 +21,7 @@ export const authBarer = async (req: Request, res: Response, next: NextFunction)
     const token = req.headers.authorization.split(' ')[1]
     const userId = await jwtService.getUserIdByToken(token)
     if (userId) {
+        //@ts-ignore
         req.user = await usersService.findUserById(userId)
         next()
     }
