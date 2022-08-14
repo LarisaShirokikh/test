a97448d755fb62889d3f4cb9b48f727a804d3499
larisasirokih <lorashirokikh@yandex.ru>
2022-08-14 09:40:26 +0300

    return all users

--- src/router/auth-router.ts
+++ src/router/auth-router.ts
@@ -14,14 +14,18 @@ export const authRouter = Router({})
 authRouter.post('/login',
     async (req: Request, res: Response) => {
         const user = await usersService.checkCredentials(req.body.login, req.body.password)
+
         if (!user) {
             res.sendStatus(401)
         } else {
+
             // @ts-ignore
+
             const findUser:  UsersWithHashType = await usersCollection.findOne({login: req.body.login})
+
             const token = await jwtService.createJWT(findUser, req.body.password)
             const userUpdate = await usersCollection
-                .updateOne({login: req.body.login}, {passwordHash: token})
+                .updateOne({login: req.body.login}, {$set: {passwordHash: token}})
             console.log(token)
             res.status(200).send(token)
         }
