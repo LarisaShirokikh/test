c9f0486c2263866e9ee8276fa2388998c848f04b
larisasirokih <lorashirokikh@yandex.ru>
2022-09-10 12:37:42 +0300

    get comment id

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -4,35 +4,33 @@ import {jwtService} from "../application/jwt-service";
 import {UserModelClass} from "../settingses/db";
 
 
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers.authorization
+    if (header === 'Basic YWRtaW46cXdlcnR5') {
+        next()
+        return
+    }
+    res.sendStatus(401)
+}
 
-
-   export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-        const header = req.headers.authorization
-        if (header === 'Basic YWRtaW46cXdlcnR5') {
-            next()
-            return
-        }
+export const authBearer = async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization) {
         res.sendStatus(401)
+        return
     }
-
-    export const authBearer = async (req: Request, res: Response, next: NextFunction) => {
-        if (!req.headers.authorization) {
-            res.sendStatus(401)
-            return
-        }
-        const token = req.headers.authorization.split(' ')[1]
-        const userId = await jwtService.getUserIdByToken(token)
-        if (userId) {
+    const token = req.headers.authorization.split(' ')[1]
+    const userId = await jwtService.getUserIdByToken(token)
+    if (userId) {
 
 
-            const user = await UserModelClass.findOne({'accountData.id': userId}, {_id: 0, __v: 0})
-            if (!user)
-                res.sendStatus(401)
-            //@ts-ignore
-            req.user = user;
-            next()
-        } else {
-            res.sendStatus(401)
-            return
-        }
+        const user = await UserModelClass.findOne({'accountData.id': userId}, {_id: 0, __v: 0})
+        if (!user) return res.sendStatus(401)
+
+        //@ts-ignore
+        req.user = user;
+        next()
+    } else {
+        res.sendStatus(401)
+        return
     }
+}
