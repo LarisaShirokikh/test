0ad9a0b3820698396e66e7f6d9e405ba00d8e0b6
larisasirokih <lorashirokikh@yandex.ru>
2022-09-11 10:25:19 +0300

    send 204 status

--- src/controllers/posts-controller.ts
+++ src/controllers/posts-controller.ts
@@ -149,7 +149,7 @@ export class PostsController {
 
         const likeStatus = await this.postsService.updateLikeStatus(req.user, req.params.postId, req.body.likeStatus)
 
-        res.status(201).send(likeStatus)
+        res.status(204).send(likeStatus)
         return
     }
 
